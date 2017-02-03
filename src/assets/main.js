d2c6ee7ae6a05eb7ac0c72f024f861a2f503ff47
swoerner <sebastian.woerner@andrena.de>
2017-02-03 16:52:04 +0100

    9: 10:

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,7 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    if(attempt.value == "" || answer.value == "") {
+    if(attempt.value == '' || answer.value == '') {
       setHiddenFields();
     }
     //add functionality to guess function here
@@ -12,9 +12,21 @@ function guess() {
 //implement new functions here
 
 function setHiddenFields() {
-  answer.value = Math.floor(Math.random() * 9999);
-  attempt.value = 0;
-  while(answer.value.toString.length < 4) {
+  answer.value = Math.floor(Math.random() * 9999).toString();
+  while(answer.value.length < 4) {
     answer.value = "0" + answer.value.toString();
   }
-};
+  attempt.value = 0;
+}
+
+function setMessage (message) {
+  document.getElementById('message').innerHTML = message;
+}
+
+function validateInput (input) {
+  if(input.toString().length == 4) {
+    return true;
+  }
+  setMessage("Guesses must be exactly 4 characters long.");
+  return false;
+}
