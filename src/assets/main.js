c67a91dd5fc2e2158733beaa480afe4cf21ab222
swoerner <sebastian.woerner@andrena.de>
2017-02-03 15:54:39 +0100

    5: set variable

--- src/assets/main.js
+++ src/assets/main.js
@@ -9,5 +9,5 @@ function guess() {
 //implement new functions here
 
 function setHiddenFields() {
-  this.answer = Math.floor(Math.random() * 9999);
+  answer.value = Math.floor(Math.random() * 9999);
 };
