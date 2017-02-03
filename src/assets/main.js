a87a63c38989f241512ad0ccae8d5c88e95f869f
swoerner <sebastian.woerner@andrena.de>
2017-02-03 17:08:48 +0100

    12:13:

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,6 +6,12 @@ function guess() {
     if(attempt.value == '' || answer.value == '') {
       setHiddenFields();
     }
+
+    if(validateInput(input.value)) {
+      attempt.value++;
+    } else {
+      return false;
+    }
     //add functionality to guess function here
 }
 
@@ -29,4 +35,24 @@ function validateInput (input) {
   }
   setMessage("Guesses must be exactly 4 characters long.");
   return false;
+}
+
+function getResults(input) {
+  var numbersCorrect = 0;
+  var html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for ( i = 0 ; i < input.length; i++) {
+    if (input.charAt(i) == answer.value.charAt(i)) {
+      html += '<span class="glyphicon glyphicon-ok"></span>'
+      numbersCorrect++;
+    } else if (answer.value.indexOf(input.charAt(i))) {
+      html += '<span class="glyphicon glyphicon-transfer"></span>'
+    } else {
+      html += '<span class="glyphicon glyphicon-remove"></span>'
+    }
+  }
+  html += '</div></div>';
+  document.getElementById('results').innerHTML += html;
+  if(numbersCorrect == 4) {
+    return true;
+  }
 }
